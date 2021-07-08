c8a6ab50d6387c30d561dacc233f97e806a68660
Denis Novozhilov <348558+gloomybrain@users.noreply.github.com>
2021-07-09 00:31:44 +0300

    (broken state) pass config service to dependent modules

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import { ConfigService } from './config.service';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!';
+
+    static get CONTENT_DISPOSITION() {
+        return 'inline';
+    }
+
+    static get ACL() {
+        return 'public-read';
+    }
+
+    private readonly s3;
+
+    constructor(private readonly config: ConfigService) {
+        this.s3 = new S3({
+            accessKeyId: this.config.awsKey,
+            secretAccessKey: this.config.awsSecret
+        });
+    }
+
+    async upload(fileName: string, file: Buffer, mimeType: string): Promise<object> {
+        const s3Response = await this.s3.upload({
+            Key: fileName,
+            Body: file,
+            ContentType: mimeType,
+            Bucket: this.config.awsS3Bucket,
+            ACL: AppService.ACL,
+            ContentDisposition: AppService.CONTENT_DISPOSITION,
+        }).promise();
+
+        return s3Response;
     }
 }
